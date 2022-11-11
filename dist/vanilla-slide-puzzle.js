07cc8f3b626c3c42c6c44a9b9b05b6cefeb99411
“Sharon <sharon.paz.00@gmail.com>
2022-11-11 12:07:09 -0800

    feat: add p5 drawing library and an image

--- dist/vanilla-slide-puzzle.js
+++ dist/vanilla-slide-puzzle.js
@@ -1,1 +1,26 @@
-console.log('vanilla javascript');
+
+// ============================
+// variables
+// ============================
+let imgSource;
+let imgWidth = 400;
+let imgHeight = 400;
+
+
+// ============================
+// P5.js Functions
+// ============================
+function preload() {
+	imgSource = loadImage("images/native-princess.jpg");
+}
+
+function setup () {
+	let canvas = createCanvas (imgWidth, imgHeight);
+	canvas.parent('canvas-puzzle');
+	frameRate(8); // 8 = slow, 24 = average (i think)
+}
+
+function draw() {
+	background(12);
+	image(imgSource, 0, 0);
+}
