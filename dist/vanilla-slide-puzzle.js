694a2b74a683f3e7fed04a1c88f7d98ee23379d5
“Sharon <sharon.paz.00@gmail.com>
2022-11-16 09:25:15 -0800

    feat(puzzle): chops puzzle board into tile pieces

--- dist/vanilla-slide-puzzle.js
+++ dist/vanilla-slide-puzzle.js
@@ -6,6 +6,23 @@ let imgSource;
 let imgWidth = 400;
 let imgHeight = 400;
 
+let tiles = [];
+let cols = 4;
+let rows = 4;
+let tileWidth, tileHeight;
+let board = []; // for tracking the tiles
+
+
+// ============================
+// Tile Constructor
+// ============================
+class Tile {
+	constructor(i, img) {
+		this.idx = i;
+		this.img = img;
+	}
+}
+
 
 // ============================
 // P5.js Functions
@@ -15,12 +32,47 @@ function preload() {
 }
 
 function setup () {
+	// canvas setup
 	let canvas = createCanvas (imgWidth, imgHeight);
 	canvas.parent('canvas-puzzle');
 	frameRate(8); // 8 = slow, 24 = average (i think)
+	// chop up source image into tiles
+	tileWidth = width / cols;
+	tileHeight = height / rows;
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			// defines section of source image to be copied to a tile
+			let x = j * tileWidth; // top coordinate for a tile
+			let y = i * tileHeight; // left coordinate for a tile
+			let idx = (i * cols) + j;
+			let tileImage = createImage(tileWidth, tileHeight);
+			// copies from image selection to destination
+			tileImage.copy(imgSource, x, y, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+			let tile = new Tile(idx, tileImage);
+			tiles.push(tile);
+			board.push(idx);
+		}
+	}
+	console.log('board', board);
 }
 
 function draw() {
 	background(12);
-	image(imgSource, 0, 0);
+	// draws each tile piece based on the board tracking array
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let x = j * tileWidth;
+			let y = i * tileHeight;
+			let idx = (i * cols) + j;
+			let tileIndex = board[idx];
+			let img = tiles[tileIndex].img;
+			image(img, x, y);
+			// draw thin lines between tile pieces
+			stroke(34);
+			strokeWeight(2);
+			noFill();
+			rect(x, y, tileWidth, tileHeight);
+		}
+	}
+	noLoop();
 }
